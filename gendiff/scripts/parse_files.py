4156d11f594610bb5d25a81693d8521745d62cad
Kloym <oleg.petr1223@gmail.com>
2024-12-21 01:39:38 +0300

    m

--- gendiff/scripts/parse_files.py
+++ gendiff/scripts/parse_files.py
@@ -1,8 +1,6 @@
 import json
 import yaml
 
-def get_file_format(file_name):
-    return file_name.split(".")[-1]
 
 def parse(file_path):
     extension = get_file_format(file_path)
@@ -12,3 +10,7 @@ def parse(file_path):
     elif extension in ("yaml", "yml"):
         with open(file_path, "r") as file:
             return yaml.safe_load(file)
+
+
+def get_file_format(file_name):
+    return file_name.split(".")[-1]
