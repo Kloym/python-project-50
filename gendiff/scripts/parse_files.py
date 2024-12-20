64a6df3265d77ed02484c6e1ea7ad5c585943f53
Kloym <oleg.petr1223@gmail.com>
2024-12-21 01:17:41 +0300

    go

--- gendiff/scripts/parse_files.py
+++ gendiff/scripts/parse_files.py
@@ -1,14 +1,5 @@
-import argparse
 import json
 import yaml
-
-
-parser = argparse.ArgumentParser(
-    description="Compares two configuration " "files and shows a difference."
-)
-parser.add_argument("first_file")
-parser.add_argument("second_file")
-parser.add_argument("-f", "--format", help="set format of output")
 
 
 def read(path):
